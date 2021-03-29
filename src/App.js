b23ff8438cf006dd12d719b34d793a7de2e35478
Алена Коломиец <alenakolomiec@MacBook-Air-Alena.local>
2021-03-29 17:34:46 +0300

    saved final

--- src/App.js
+++ src/App.js
@@ -1,102 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList";
 import Filter from "./components/Filter/Filter";
+import {
+  addContact,
+  deleteContact,
+  changeFilter,
+  initialContacts,
+} from "./redux/toolkit/actions/contacts";
 
-
-
-
-
-//   componentDidMount() {
-//     try {
-//       const contactsFromStorage = JSON.parse(localStorage.getItem("contacts"));
-//       contactsFromStorage && this.setState({ contacts: contactsFromStorage });
-//     } catch {
-//       console.log("Невалидный JSON");
-//     }
-//   }
-
-//   inputHandler = ({ target }) => {
-//     const { value, name } = target;
-
-//     this.setState({ [name]: value });
-//   };
-
-
-
-//   filterContacts = () => {
-//     const { contacts, filter } = this.state;
-//     const insensitiveValue = filter.toLowerCase();
-//     if (filter.length) {
-//       return contacts.filter((contact) =>
-//         contact.name.toLowerCase().includes(insensitiveValue)
-//       );
-//     } else {
-//       return contacts;
-//     }
-//   };
-//   deleteContact = (id) => {
-//     const { contacts } = this.state;
-//     this.setState({
-//       contacts: contacts.filter((contact) => contact.id !== id),
-//     });
-//   };
-//   render() {
-//     const { filter } = this.state;
-//     const filteredContacts = this.filterContacts();
-//     return (
-//       <div>
-//         <h1>Phonebook</h1>
-//         <ContactForm addToPhonebook={this.addToPhonebook} />
-//         <h2>Contacts</h2>
-//         <Filter filter={filter} inputHandler={this.inputHandler} />
-//         <ContactList
-//           filteredContacts={filteredContacts}
-//           deleteContact={this.deleteContact}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-const initialState = {
-  contacts: [
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ],
-  filter: "",
-};
+// const initialState = {
+//   contacts: [
+//     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+//   ],
+//   filter: "",
+// };
 
 const App = () => {
-  const contactsLocalStorage = localStorage.getItem("contacts");
-  const [contacts, setContacts] = useState(
-    contactsLocalStorage
-      ? JSON.parse(contactsLocalStorage)
-      : [...initialState.contacts]
-  );
+  // const [contacts, setContacts] = useState([...initialState.contacts]);
+  // const [filter, setFilter] = useState(initialState.filter);
+  const contacts = useSelector((state) => state.contacts.items);
+  const filter = useSelector((state) => state.contacts.filter);
+  const dispatch = useDispatch();
 
+  const inputHandler = ({ target }) => {
+    const { value } = target;
+    // setFilter(value);
+    dispatch(changeFilter(value));
+  };
 
-
-//   componentDidMount() {
-//     try {
-//       const contactsFromStorage = JSON.parse(localStorage.getItem("contacts"));
-//       contactsFromStorage && this.setState({ contacts: contactsFromStorage });
-//     } catch {
-//       console.log("Невалидный JSON");
-//     }
-//   }
-
-//   inputHandler = ({ target }) => {
-//     const { value, name } = target;
-
-//     this.setState({ [name]: value });
-//   };
-
-  const [filter, setFilter] = useState(initialState.filter);
+  useEffect(() => {
+    try {
+      const contactsLocalStorage = localStorage.getItem("contacts");
+      // setContacts(
+      //   contactsLocalStorage
+      //     ? JSON.parse(contactsLocalStorage)
+      //     : [...initialState.contacts]
+      // );
+      contactsLocalStorage &&
+        dispatch(initialContacts(JSON.parse(contactsLocalStorage)));
+    } catch {
+      console.log("Невалидный JSON");
+    }
+  }, []);
 
   const filterContacts = () => {
     if (!filter.length) {
@@ -113,32 +63,29 @@ const App = () => {
     );
   };
 
-  const addContact = (contactObj) => {
+  const add = (contactObj) => {
     uniqueContact(contactObj.name)
       ? alert(`${contactObj.name} is already in contacts`)
-      : setContacts((prev) => [...prev, contactObj]);
+      : //   : setContacts((prev) => [...prev, contactObj]);
+        dispatch(addContact(contactObj));
   };
-
 
   const deleteContacts = (id) => {
-    setContacts((prev) => prev.filter((contact) => contact.id !== id));
+    // setContacts((prev) => prev.filter((contact) => contact.id !== id));
+    dispatch(deleteContact(id));
   };
-
-
 
   useEffect(() => {
     localStorage.setItem("contacts", JSON.stringify(contacts));
   }, [contacts]);
 
-
-
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm addContact={addContact} />
+      <ContactForm addToPhonebook={add} />
 
       <h2>Contacts</h2>
-      <Filter filter={filter} setFilter={setFilter} />
+      <Filter filter={filter} inputHandler={inputHandler} />
       <ContactList
         filteredContacts={filterContacts()}
         deleteContact={deleteContacts}

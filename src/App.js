8604f68f0d2d1fdcb8170a9359c1676f91650290
Алена Коломиец <alenakolomiec@MacBook-Air-Alena.local>
2021-03-29 12:22:03 +0300

    saved

--- src/App.js
+++ src/App.js
@@ -1,85 +1,150 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList";
 import Filter from "./components/Filter/Filter";
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
-    filter: "",
-  };
 
-  componentDidUpdate(prevProps, prevState) {
-    const nextContacts = this.state.contacts;
-    const prevContacts = prevState.contacts;
-    if (nextContacts !== prevContacts) {
-      localStorage.setItem("contacts", JSON.stringify(nextContacts));
-    }
-  }
 
-  componentDidMount() {
-    try {
-      const contactsFromStorage = JSON.parse(localStorage.getItem("contacts"));
-      contactsFromStorage && this.setState({ contacts: contactsFromStorage });
-    } catch {
-      console.log("Невалидный JSON");
-    }
-  }
 
-  inputHandler = ({ target }) => {
-    const { value, name } = target;
 
-    this.setState({ [name]: value });
-  };
+//   componentDidMount() {
+//     try {
+//       const contactsFromStorage = JSON.parse(localStorage.getItem("contacts"));
+//       contactsFromStorage && this.setState({ contacts: contactsFromStorage });
+//     } catch {
+//       console.log("Невалидный JSON");
+//     }
+//   }
 
-  addToPhonebook = (newContact) => {
-    this.state.contacts.find(
-      (contact) => contact.name.toLowerCase() === newContact.name.toLowerCase()
-    )
-      ? alert(`${newContact.name} is already in contacts`)
-      : this.setState((prev) => ({
-          contacts: [...prev.contacts, newContact],
-        }));
-  };
+//   inputHandler = ({ target }) => {
+//     const { value, name } = target;
 
-  filterContacts = () => {
-    const { contacts, filter } = this.state;
-    const insensitiveValue = filter.toLowerCase();
-    if (filter.length) {
-      return contacts.filter((contact) =>
-        contact.name.toLowerCase().includes(insensitiveValue)
-      );
-    } else {
+//     this.setState({ [name]: value });
+//   };
+
+
+
+//   filterContacts = () => {
+//     const { contacts, filter } = this.state;
+//     const insensitiveValue = filter.toLowerCase();
+//     if (filter.length) {
+//       return contacts.filter((contact) =>
+//         contact.name.toLowerCase().includes(insensitiveValue)
+//       );
+//     } else {
+//       return contacts;
+//     }
+//   };
+//   deleteContact = (id) => {
+//     const { contacts } = this.state;
+//     this.setState({
+//       contacts: contacts.filter((contact) => contact.id !== id),
+//     });
+//   };
+//   render() {
+//     const { filter } = this.state;
+//     const filteredContacts = this.filterContacts();
+//     return (
+//       <div>
+//         <h1>Phonebook</h1>
+//         <ContactForm addToPhonebook={this.addToPhonebook} />
+//         <h2>Contacts</h2>
+//         <Filter filter={filter} inputHandler={this.inputHandler} />
+//         <ContactList
+//           filteredContacts={filteredContacts}
+//           deleteContact={this.deleteContact}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+const initialState = {
+  contacts: [
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ],
+  filter: "",
+};
+
+const App = () => {
+  const contactsLocalStorage = localStorage.getItem("contacts");
+  const [contacts, setContacts] = useState(
+    contactsLocalStorage
+      ? JSON.parse(contactsLocalStorage)
+      : [...initialState.contacts]
+  );
+
+
+
+//   componentDidMount() {
+//     try {
+//       const contactsFromStorage = JSON.parse(localStorage.getItem("contacts"));
+//       contactsFromStorage && this.setState({ contacts: contactsFromStorage });
+//     } catch {
+//       console.log("Невалидный JSON");
+//     }
+//   }
+
+//   inputHandler = ({ target }) => {
+//     const { value, name } = target;
+
+//     this.setState({ [name]: value });
+//   };
+
+  const [filter, setFilter] = useState(initialState.filter);
+
+  const filterContacts = () => {
+    if (!filter.length) {
       return contacts;
+    } else {
+      return contacts.filter((elem) =>
+        elem.name.toLowerCase().includes(filter.toLowerCase())
+      );
     }
   };
-  deleteContact = (id) => {
-    const { contacts } = this.state;
-    this.setState({
-      contacts: contacts.filter((contact) => contact.id !== id),
-    });
-  };
-  render() {
-    const { filter } = this.state;
-    const filteredContacts = this.filterContacts();
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm addToPhonebook={this.addToPhonebook} />
-        <h2>Contacts</h2>
-        <Filter filter={filter} inputHandler={this.inputHandler} />
-        <ContactList
-          filteredContacts={filteredContacts}
-          deleteContact={this.deleteContact}
-        />
-      </div>
+  const uniqueContact = (name) => {
+    return contacts.some(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
     );
-  }
-}
+  };
+
+  const addContact = (contactObj) => {
+    uniqueContact(contactObj.name)
+      ? alert(`${contactObj.name} is already in contacts`)
+      : setContacts((prev) => [...prev, contactObj]);
+  };
+
+
+  const deleteContacts = (id) => {
+    setContacts((prev) => prev.filter((contact) => contact.id !== id));
+  };
+
+
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+
+      <h2>Contacts</h2>
+      <Filter filter={filter} setFilter={setFilter} />
+      <ContactList
+        filteredContacts={filterContacts()}
+        deleteContact={deleteContacts}
+      />
+    </div>
+  );
+};
 
 export default App;
--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,64 +1,129 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import styles from "./ContactForm.module.css";
 import PropTypes from "prop-types";
 
-class ContactForm extends Component {
-  initialState = {
-    name: "",
-    number: "",
+// class ContactForm extends Component {
+//   initialState = {
+//     name: "",
+//     number: "",
+//   };
+//   state = {
+//     name: "",
+//     number: "",
+//   };
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     const singleContact = {
+//       name: this.state.name,
+//       number: this.state.number,
+//       id: uuidv4(),
+//     };
+//     this.props.addToPhonebook(singleContact);
+//     this.setState({ ...this.initialState });
+//   };
+
+//   inputHandler = ({ target }) => {
+//     const { value, name } = target;
+
+//     this.setState({ [name]: value });
+//   };
+
+//   render() {
+//     const { name, number } = this.state;
+//     return (
+//       <>
+//         <form onSubmit={this.handleSubmit}>
+//           <label className={styles.contactFormLable}>
+//             <input
+//               onChange={this.inputHandler}
+//               type="text"
+//               name="name"
+//               placeholder="Enter name..."
+//               value={name}
+//             ></input>
+//           </label>
+//           <label className={styles.contactFormLable}>
+//             <input
+//               onChange={this.inputHandler}
+//               type="tel"
+//               name="number"
+//               placeholder="Enter number..."
+//               value={number}
+//             ></input>
+//           </label>
+
+//           <button type="submit">Add contact</button>
+//         </form>
+//       </>
+//     );
+//   }
+// }
+
+// ContactForm.propTypes = {
+//   addToPhonebook: PropTypes.func.isRequired,
+// };
+
+// export default ContactForm;
+
+const initialState = {
+  name: "",
+  number: "",
+};
+
+const ContactForm = ({ addToPhonebook }) => {
+  const [{ name, number }, setForm] = useState({
+    ...initialState,
+  });
+
+  const inputHandler = ({ target }) => {
+    const { value, name } = target;
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
   };
-  state = {
-    name: "",
-    number: "",
-  };
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const singleContact = {
-      name: this.state.name,
-      number: this.state.number,
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const contact = {
+      name,
+      number,
       id: uuidv4(),
     };
-    this.props.addToPhonebook(singleContact);
-    this.setState({ ...this.initialState });
+
+    addToPhonebook(contact);
+
+    setForm(initialState);
   };
 
-  inputHandler = ({ target }) => {
-    const { value, name } = target;
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <label className={styles.contactFormLable}>
+          <input
+            onChange={inputHandler}
+            type="text"
+            name="name"
+            placeholder="Enter name..."
+            value={name}
+          ></input>
+        </label>
+        <label className={styles.contactFormLable}>
+          <input
+            onChange={inputHandler}
+            type="tel"
+            name="number"
+            placeholder="Enter number..."
+            value={number}
+          ></input>
+        </label>
 
-    this.setState({ [name]: value });
-  };
-
-  render() {
-    const { name, number } = this.state;
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <label className={styles.contactFormLable}>
-            <input
-              onChange={this.inputHandler}
-              type="text"
-              name="name"
-              placeholder="Enter name..."
-              value={name}
-            ></input>
-          </label>
-          <label className={styles.contactFormLable}>
-            <input
-              onChange={this.inputHandler}
-              type="tel"
-              name="number"
-              placeholder="Enter number..."
-              value={number}
-            ></input>
-          </label>
-
-          <button type="submit">Add contact</button>
-        </form>
-      </>
-    );
-  }
-}
+        <button type="submit">Add contact</button>
+      </form>
+    </div>
+  );
+};
 
 ContactForm.propTypes = {
   addToPhonebook: PropTypes.func.isRequired,
